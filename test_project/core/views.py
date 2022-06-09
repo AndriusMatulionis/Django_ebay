8a367cd40cf434d6bdf57c66b2639376800bc3a7
AndriusMatulionis <89296933+AndriusMatulionis@users.noreply.github.com>
2022-06-09 13:25:56 +0300

    prijungtas html ir css

--- test_project/core/views.py
+++ test_project/core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Scrape
+
+def all_scraped_data(request):
+    scraped_list = Scrape.objects.all()
+    render(request, 'core/templates/index.html',
+        {'scraped_list': scraped_list })
 
 def index(request):
-    return HttpResponse("Hello")
+    
+    return render(request,'index.html')
 
